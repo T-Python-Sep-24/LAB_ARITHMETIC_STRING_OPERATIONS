67e2612d5aeaa613add188f24709d1adb068cd56
HmzhDubh <meezo.2015@hotmail.com>
2024-10-01 18:35:00 +0300

    completed bonus work

---
+++ lab02_bonus.py
@@ -0,0 +1,11 @@
+
+mySentence = "Hello world my name is Hmzh and I am currently working on Python and Web development with Tuwaiq Academy"
+searchWord = "Hmzh"
+
+print(len(mySentence))
+print(mySentence.find(searchWord))
+print(mySentence.count(searchWord))
+print(mySentence.upper())
+print(mySentence.lower())
+print(mySentence.replace(searchWord, "Hamzah Dabbah"))
+print(mySentence[-1])
