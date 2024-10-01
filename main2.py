e5380021bbe5893521d99c551463abff1b340cf4
Mohamed <sirexplq@gmail.com>
2024-10-01 19:03:26 +0300

    solvedlab2

---
+++ main2.py
@@ -0,0 +1,10 @@
+s_v="Creating a responsive website requires HTML, CSS, JavaScript, and teamwork"
+s_v2= "website"
+print(len(s_v))
+print(s_v.index(s_v2))
+print(s_v.count(s_v2))
+print(s_v.upper())
+print(s_v.lower())
+s_v3="web"
+print(s_v.replace(s_v2,s_v3))
+print(s_v[-1])
