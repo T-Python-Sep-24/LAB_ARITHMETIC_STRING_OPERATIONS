00c714e87d4d59ee1d604ccca0323a524e6cb42f
Shouq <shouq000@hotmail.com>
2024-10-01 19:43:49 +0300

    LAB_ARITHMETIC_STRING_OPERATIONS

---
+++ Bonusfile1.py
@@ -0,0 +1,9 @@
+name:str="Shouq Abdullah"
+var:str="My name is " + name
+print(len(name))
+print(name.index("Shouq"))
+print(name.count("Shouq"))
+print(name.upper())
+print(name.lower())
+name=name.replace("Abdullah","Alsubaie")
+print(name[-1])
