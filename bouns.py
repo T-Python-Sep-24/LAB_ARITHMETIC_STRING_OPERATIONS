6a53c06b47c29e7eacf3e8b3b0af0510b917fd62
mishal <dzl15959@gmail.com>
2024-10-01 18:06:04 +0300

    solved bouns lab

---
+++ bouns.py
@@ -0,0 +1,18 @@
+python = "Python is a high-level open-source programming language"
+language = "Python"
+
+print(len(python))
+
+print(python.index(language))
+
+print(python.count(language))
+
+print(python.upper())
+
+print(python.lower())
+
+
+new_sentence = python.replace(language, "java")
+print(new_sentence)
+
+print(python[-1])
---
+++ main.py
@@ -0,0 +1,17 @@
+
+price = 2.99  
+quantity = 3  
+tax_rate = 0.075 
+
+subtotal = price * quantity
+
+tax = subtotal * tax_rate
+
+total = subtotal + tax
+
+print(f"Price of item: ${price:.2f}")
+print(f"Quantity: {quantity}")
+print(f"Tax rate: {tax_rate * 100:.1f}%\n")
+print(f"Subtotal: $ {subtotal:.2f}")
+print(f"Tax: ${tax:.2f}")
+print(f"Total: ${total:.2f}")
